10866cbba7b6d9db5ce8562febeb0010d1f54421
akzy1991 <richo_koh@hotmail.com>
2020-05-20 13:12:22 +0000

    complete qn6

--- 06-post-requests/app.py
+++ 06-post-requests/app.py
@@ -6,11 +6,27 @@ app = Flask(__name__)
 # your code here!
 @app.route("/")
 def hello():
-	return render_template("index.template.html")
-	
-@app.route("/",methods=['POST'])
+    return render_template("index.template.html")
+
+
+@app.route("/", methods=['POST'])
 def processHello():
-	return render_template('process-hello.template.html')
+    fn = request.form.get('first-name')
+    ln = request.form.get('last-name')
+    return render_template('process-hello.template.html', fn=fn, ln=ln)
+
+
+@app.route("/calculate")
+def calculate():
+    return render_template("calculate.template.html")
+
+
+@app.route("/calculate", methods=['POST'])
+def calculate_results():
+    number1 = int(request.form.get('number1'))
+    number2 = int(request.form.get('number2'))
+    sum = number1 + number2
+    return render_template("results.template.html", result=sum)
 
 
 # "magic code" -- boilerplate
